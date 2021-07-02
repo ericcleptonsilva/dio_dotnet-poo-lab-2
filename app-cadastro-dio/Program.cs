17d4be830ef852af2e2d9d5f5e6e3b0f0e3982e1
ericcleptonsilva <ericcleptonsilva@gmail.com>
2021-07-02 13:46:38 -0300

    Projeto Creado pela Command-line

---
+++ app-cadastro-dio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace app_cadastro_dio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
